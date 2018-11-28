caf00b1db6145ad6ac1e272308654a582b9165dc
Jeffrey Russom <jeffreyrussom@gmail.com>
2018-11-27 22:46:29 -0800

    Checkpoint

--- src/Recipe.js
+++ src/Recipe.js
@@ -66,19 +66,16 @@ class Recipe extends React.Component {
             instructions
         } = Recipes[0];
         return (
-            <Flex flexWrap="wrap" m="auto" css={{ maxWidth: "72em" }}>
-                <Box p="2" width={[1, 1, 4 / 5]}>
-                    <Card
-                        bg="white"
-                        borderRadius={3}
-                        boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
-                        css={{ overflow: "hidden" }}
-                    >
-                        <Image
-                            src="http://demo.djmimi.net/themes/recipe/wp-content/uploads/2015/02/france-confectionery-83373-848x477.jpg"
-                            width={[1]}
-                        />
-                        <Box>
+            // <Flex flexWrap="wrap" m="auto" css={{ maxWidth: "72em" }}>
+            <Box p="2" width={[1, 1, 1]} m="auto" css={{ maxWidth: "64em" }}>
+                <Card
+                    bg="white"
+                    borderRadius={3}
+                    boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
+                    css={{ overflow: "hidden" }}
+                >
+                    <Flex flexWrap="wrap">
+                        <Box width={[1, 1, 1, 0.4]}>
                             <Heading m="4" as="h1" color="gray.1">
                                 {title}
                             </Heading>
@@ -90,153 +87,96 @@ class Recipe extends React.Component {
                             >
                                 {longDescription}
                             </Text>
-
-                            <Flex
-                                m="4"
-                                css={{ borderTop: "1px solid #eee" }}
-                                flexWrap="wrap"
-                            >
-                                <Box width={[1, 1, 1, 1 / 2]} mt="4">
-                                    <Heading
-                                        as="h4"
-                                        color="gray.1"
-                                        fontSize="4"
-                                        fontWeight="normal"
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faEyeDropper}
-                                            color="#6ba72b"
-                                        />
-                                        <RightSpan>Ingredients</RightSpan>
-                                    </Heading>
-                                    <List>
-                                        {ingredients.map(
-                                            (ingredient, index) => {
-                                                return (
-                                                    <NakedLi key={index}>
-                                                        <Checkbox
-                                                            checked={
-                                                                this.state
-                                                                    .checked[
-                                                                    index
-                                                                ]
-                                                            }
-                                                            onCheck={this.handleIngredientChecked(
-                                                                index
-                                                            )}
-                                                            label={ingredient}
-                                                        />
-                                                    </NakedLi>
-                                                );
-                                            }
-                                        )}
-                                    </List>
-                                </Box>
-                                <Box width={[1, 1, 1, 1 / 2]} mt="4">
-                                    <Heading
-                                        as="h4"
-                                        color="gray.1"
-                                        fontSize="4"
-                                        fontWeight="normal"
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faSortNumericDown}
-                                            color="#6ba72b"
-                                        />
-                                        <RightSpan>Steps</RightSpan>
-                                    </Heading>
-                                    <List>
-                                        {instructions.map(
-                                            (instruction, index) => {
-                                                return (
-                                                    <NakedLi key={index}>
-                                                        <Checkbox
-                                                            align="top"
-                                                            checked={
-                                                                this.state
-                                                                    .checked[
-                                                                    index +
-                                                                        recipe
-                                                                            .ingredients
-                                                                            .length
-                                                                ]
-                                                            }
-                                                            onCheck={this.handleStepChecked(
-                                                                index
-                                                            )}
-                                                            label={`Step ${index +
-                                                                1}\n${instruction}`}
-                                                        />
-                                                    </NakedLi>
-                                                );
-                                            }
-                                        )}
-                                    </List>
-                                </Box>
-                            </Flex>
                         </Box>
-                    </Card>
-                </Box>
-                <Flex p="2" width={[1, 1, 1 / 5]} flexDirection="column">
-                    <Card
-                        p={3}
-                        bg="white"
-                        borderRadius={3}
-                        boxShadow="0 1px 2px rgba(0, 0, 0, 0.1)"
-                    >
-                        <List>
-                            <FlexLi>
-                                <Text
+
+                        <Image
+                            src="http://demo.djmimi.net/themes/recipe/wp-content/uploads/2015/02/france-confectionery-83373-848x477.jpg"
+                            width={[1, 1, 1, 0.6]}
+                            height="100%"
+                        />
+                    </Flex>
+
+                    <Box>
+                        <Flex
+                            m="4"
+                            css={{ borderTop: "1px solid #eee" }}
+                            flexWrap="wrap"
+                        >
+                            <Box width={[1, 1, 1, 1 / 2]} mt="4">
+                                <Heading
+                                    as="h4"
                                     color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
+                                    fontSize="4"
+                                    fontWeight="normal"
                                 >
-                                    Yield
-                                </Text>
-                                <Text
+                                    <FontAwesomeIcon
+                                        icon={faEyeDropper}
+                                        color="#6ba72b"
+                                    />
+                                    <RightSpan>Ingredients</RightSpan>
+                                </Heading>
+                                <List>
+                                    {ingredients.map((ingredient, index) => {
+                                        return (
+                                            <NakedLi key={index}>
+                                                <Checkbox
+                                                    checked={
+                                                        this.state.checked[
+                                                            index
+                                                        ]
+                                                    }
+                                                    onCheck={this.handleIngredientChecked(
+                                                        index
+                                                    )}
+                                                    label={ingredient}
+                                                />
+                                            </NakedLi>
+                                        );
+                                    })}
+                                </List>
+                            </Box>
+                            <Box width={[1, 1, 1, 1 / 2]} mt="4">
+                                <Heading
+                                    as="h4"
                                     color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
+                                    fontSize="4"
+                                    fontWeight="normal"
                                 >
-                                    {recipe.recipeYield}
-                                </Text>
-                            </FlexLi>
-                            <FlexLi>
-                                <Text
-                                    color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
-                                >
-                                    Servings
-                                </Text>
-                                <Text
-                                    color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
-                                >
-                                    {recipe.servings}
-                                </Text>
-                            </FlexLi>
-                            <FlexLi>
-                                <Text
-                                    color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
-                                >
-                                    Cook Time
-                                </Text>
-                                <Text
-                                    color="gray.1"
-                                    lineHeight="1.8"
-                                    fontSize="1"
-                                >
-                                    {recipe.cookTime}
-                                </Text>
-                            </FlexLi>
-                        </List>
-                    </Card>
-                </Flex>
-            </Flex>
+                                    <FontAwesomeIcon
+                                        icon={faSortNumericDown}
+                                        color="#6ba72b"
+                                    />
+                                    <RightSpan>Steps</RightSpan>
+                                </Heading>
+                                <List>
+                                    {instructions.map((instruction, index) => {
+                                        return (
+                                            <NakedLi key={index}>
+                                                <Checkbox
+                                                    align="top"
+                                                    checked={
+                                                        this.state.checked[
+                                                            index +
+                                                                recipe
+                                                                    .ingredients
+                                                                    .length
+                                                        ]
+                                                    }
+                                                    onCheck={this.handleStepChecked(
+                                                        index
+                                                    )}
+                                                    label={`Step ${index +
+                                                        1}\n${instruction}`}
+                                                />
+                                            </NakedLi>
+                                        );
+                                    })}
+                                </List>
+                            </Box>
+                        </Flex>
+                    </Box>
+                </Card>
+            </Box>
         );
     }
 }
